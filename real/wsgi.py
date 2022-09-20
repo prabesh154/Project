5c780cb5527b3efd0917475aa74bcca5b76a850f
prabesh <prabeshbizz@gmail.com>
2022-09-20 21:17:02 +0545

    Static files, Templates , Listings and Realtors were created

--- real/wsgi.py
+++ real/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real.settings')
 
 application = get_wsgi_application()
+#show web server communicates with web application
