be37b2c97e9662e1bb6cecb4378f0e90c0794292
prabesh <prabeshbizz@gmail.com>
2022-09-27 22:38:13 +0545

    Added dictionaries in choices for search

---
+++ listings/choices.py
@@ -0,0 +1,42 @@
+bedroom_choices ={
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10,
+    '11':11,
+    '12':12,
+    '13':13,
+
+}
+
+
+price_choices ={
+
+    '1crore':'1crore',
+    '2crore':'2crore',
+    '3crore':'3crore',
+    '4crore':'4crore',
+    '5crore':'5crore',
+    '6crore':'6crore',
+    '7crore':'7crore',
+    '8crore':'8crore',
+    '9crore':'9crore',
+    '10crore+':'10crore+',
+}
+
+district_choices={
+    'KTM': 'Kathmandu',
+    'LLP': 'Lalitpur',
+    'BKT': 'Bhaktapur',
+    'CHW': 'Chitwan',
+    'PKR':'Kaski/Pokhara',
+    'BHR': 'Bhairahawa',
+    'KAV':'Kavre'
+}
+   
