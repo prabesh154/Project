6170b16e9b58c71770b3833acf08d7256afee0e5
prabesh <prabeshbizz@gmail.com>
2022-10-02 12:37:31 +0545

    Email feature added

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from contacts.models import Contact
-
+from django.core.mail import send_mail
 # Create your views here.
 def contact(request):
     if request.method == 'POST':
@@ -14,12 +14,29 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+     #Check if user has made inquiry already:
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_connected = Contact.objects.all().filter(listing_id=listing_id,user_id=user_id)
+            if has_connected:
+                messages.error(request,'You have already made an inquiry for this listing')
+                return redirect ('/listings/'+listing_id)
+
         contact = Contact(listing=listing,listing_id=listing_id,name=name,email=email,
         phone=phone,message=message,user_id=user_id )
-
         contact.save()
 
-        messages.success(request,'Your request has been submitted , a realtor will get back to you in a while')
+        #send maul
+        send_mail (
+            'Property Listing Inquiry',
+            'There has been an inquiry for '+listing+'.Sign into the admin panel for more info',
+            'rijalprabesh154@gmail.com',
+            [realtor_email,'rijalprabesh145@gmail.com'],
+            fail_silently=False
+
+        )
+
+        messages.success(request,'Your request has been submitted. A realtor will get back to you in a while')
         return redirect ('/listings/'+listing_id)
       
     
